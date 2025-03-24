0857ed25a8a1379a4e9f1aec0d307d772c09bc1c
RUAN BRÖERING RODRIGUES <ruan_rodrigues3@estudante.sesisenai.org.br>
2025-03-24 09:18:16 -0300

    asd

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ class Snake {
     constructor() {
         this.parts = [new SnakePart(100, 200, 20, 20)];
         this.direction = 'right';
+        this.speed = 7;
     }
 
     update() {
@@ -50,16 +51,16 @@ class Snake {
 
         switch (this.direction) {
             case 'up':
-                newY -= 7;
+                newY -= this.speed;
                 break;
             case 'down':
-                newY += 7;
+                newY += this.speed;
                 break;
             case 'left':
-                newX -= 7;
+                newX -= this.speed;
                 break;
             case 'right':
-                newX += 7;
+                newX += this.speed;
                 break;
         }
 
@@ -102,18 +103,22 @@ class Snake {
     }
 
     #comer(comida) {
-        const newPart = new SnakePart(comida.x, comida.y, 20, 20);
-        this.parts.push(newPart);
         comida.x = Math.floor(Math.random() * (canvas.width - 20));
         comida.y = Math.floor(Math.random() * (canvas.height - 20));
 
-        // Update the score
         const scoreElement = document.getElementById('score');
         const score = parseInt(scoreElement.innerText.split(': ')[1]) + 1;
         scoreElement.innerText = `Score: ${score}`;
+
+        const recordElement = document.getElementById('record');
+        const record = parseInt(localStorage.getItem('record')) || 0;
+
+        if (score > record) {
+            localStorage.setItem('record', score);
+            recordElement.innerText = `Record: ${score}`;
+        }
     }
 }
-
 class Comida extends Entidade {
     constructor() {
         super(Math.random() * (canvas.width - 20), Math.random() * (canvas.height - 20), 20, 20);
@@ -131,6 +136,30 @@ function loop() {
     snake.draw();
     comida.desenhar();
     snake.checkCollision(comida);
+
+    const scoreElement = document.getElementById('score');
+    const score = parseInt(scoreElement.innerText.split(': ')[1]);
+    scoreElement.innerText = `Score: ${score}`;
+
+    const recordElement = document.getElementById('record');
+    const record = parseInt(localStorage.getItem('record')) || 0;
+    recordElement.innerText = `Record: ${record}`;
+
+    const canvasWidth = canvas.width;
+    const canvasHeight = canvas.height;
+    const snakeHead = snake.parts[0];
+
+    if (snakeHead.x < 0 || snakeHead.x >= canvasWidth || snakeHead.y < 0 || snakeHead.y >= canvasHeight) {
+
+        alert(`Game Over! Score: ${score}`);
+
+        scoreElement.innerText = 'Score: 0';
+        recordElement.innerText = 'Record: 0';
+        snake.parts = [new SnakePart(100, 200, 20, 20)];
+        comida.x = Math.floor(Math.random() * (canvasWidth - 20));
+        comida.y = Math.floor(Math.random() * (canvasHeight - 20));
+    }
+
     requestAnimationFrame(loop);
 }
 
